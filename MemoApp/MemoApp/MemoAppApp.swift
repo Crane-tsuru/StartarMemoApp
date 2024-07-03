0da8617858504358d24d87607f93b7b9f7cd28ae
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-07-03 22:30:58 +0900

    add xcode project to remote repository

---
+++ MemoApp/MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by 鶴見駿 on 2024/07/03.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
