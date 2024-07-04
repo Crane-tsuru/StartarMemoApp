242685a06b367e7792cc960a9bb83ec71a7423e0
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-07-04 14:03:23 +0900

    createb HomeView.swift

---
+++ MemoApp/MemoApp/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MemoApp
+//
+//  Created by 鶴見駿 on 2024/07/04.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
